b508bfa417cf916ae8c3472dfffe91983ac1dbc5
mouliswaran <104911211+mouliswaran@users.noreply.github.com>
2022-08-26 11:24:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,9 @@
-terraform {
-
-  required_version = ">=0.12"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
 
 locals {
-  resource_group = "Kennametal"
+  resource_group = "Kennametal-poc"
   location       = "East US"
 }
 
